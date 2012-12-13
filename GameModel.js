06f7b507c09fb62bd26ca48e0d3d7b74051132bc
Lance Pioch <lance@lancepioch.com>
2012-12-12 23:11:55 -0500

    Initial Commit

---
+++ GameModel.js
@@ -0,0 +1,35 @@
+function GameModel( {
+	// this.name = name;
+	this.playerCount = 2;
+	this.currentPlayer = 0;
+};
+
+GameModel.prototype.getNextPlayer = 
+	function () {
+		var nextPlayer = this.currentPlayer + 1;
+		if (nextPlayer >= this.playerCount)
+			nextPlayer = 0;
+		return nextPlayer;
+	};
+
+GameModel.prototype.currentPlayerHasLegalMove = 
+	function () {
+		return true;
+	};
+
+GameModel.prototype.nextTurn = 
+	function () {
+		this.currentPlayer = this.getNextPlayer();
+	}
+
+GameModel.prototype.toString = 
+	function () {
+		return "Standard Game Model";
+	};
+
+GameModel.prototype.equals = 
+	function (other) {
+		return false;
+	};
+
+// public abstract void startNewGame();
---
+++ README.md
@@ -0,0 +1,1 @@
+Node.js Game Library
