12c006c1e3e7f73d1dcdad093075856fdcac7e9b
Lance Pioch <lance@lancepioch.com>
2012-12-13 11:38:35 -0500

    class method syntax

--- GameModel.js
+++ GameModel.js
@@ -1,35 +1,33 @@
-function GameModel( {
+function GameModel() {
 	// this.name = name;
 	this.playerCount = 2;
 	this.currentPlayer = 0;
 };
 
-GameModel.prototype.getNextPlayer = 
-	function () {
-		var nextPlayer = this.currentPlayer + 1;
-		if (nextPlayer >= this.playerCount)
-			nextPlayer = 0;
-		return nextPlayer;
-	};
+GameModel.prototype.getNextPlayer = function () {
+	var nextPlayer = this.currentPlayer + 1;
+	if (nextPlayer >= this.playerCount)
+		nextPlayer = 0;
+	return nextPlayer;
+};
 
-GameModel.prototype.currentPlayerHasLegalMove = 
-	function () {
-		return true;
-	};
+GameModel.prototype.currentPlayerHasLegalMove = function () {
+	return true;
+};
 
-GameModel.prototype.nextTurn = 
-	function () {
-		this.currentPlayer = this.getNextPlayer();
-	}
+GameModel.prototype.nextTurn = function () {
+	this.currentPlayer = this.getNextPlayer();
+}
 
-GameModel.prototype.toString = 
-	function () {
-		return "Standard Game Model";
-	};
+GameModel.prototype.toString = function () {
+	return "Standard Game Model";
+};
 
-GameModel.prototype.equals = 
-	function (other) {
-		return false;
-	};
+GameModel.prototype.equals = function (other) {
+	return false;
+};
 
-// public abstract void startNewGame();
+
+// Usage
+/* var Inforia = require('./Inforia.js');
+var gamemodel = new Inforia.GameModel('Will'); //*/
