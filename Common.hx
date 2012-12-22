bb6ba99422bb2d2cb6b93b780f7fdb711e9c5e02
Lance Pioch <me@lancepioch.com>
2012-12-21 22:14:29 -0500

    Added and tested code for socket.io websockets along with test html

---
+++ Common.hx
@@ -0,0 +1,29 @@
+package;
+
+// https://github.com/kyubuns/haxe-socketio-chat/blob/master/Common.hx
+
+@:native("io")
+extern class IO {
+  static function connect(host:String, ?option:Dynamic):Socket;
+}
+
+extern class SocketIO {
+  var sockets : Sockets;
+  function set(name:String, option:Dynamic):Void;
+}
+
+extern class Sockets {
+  function on(event:String, handler:Socket->Void) : Void;
+}
+
+extern class Socket {
+  function emit(event:String, data:Dynamic) : Void;
+  @:overload(function (event:String, handler:Void->Void):Void{})
+  function on(event:String, handler:Dynamic->Void) : Void;
+  function disconnect() : Void;
+}
+
+@:native("setInterval")
+extern class SetInterval {
+  function new(f:Void->Void, interval:Int) : Void;
+}
--- Inforia.hx
+++ Inforia.hx
@@ -1,6 +1,10 @@
 import js.Node;
+// import js.node.SocketIo;
+// import js.node.SocketIoClient;
 import GameController;
 import ToadsAndFrogsModel;
+import Common;
+import Std;
 
 class Inforia {
 	public static function main(){
@@ -8,7 +12,7 @@ class Inforia {
 		var gc = new GameController(gm);
 		gc.startNewGame();
 
-		var server = Node.http.createServer (
+		var app = Node.http.createServer (
 			function(req : NodeHttpServerReq, res : NodeHttpServerResp) {
 				var url_parts = Node.url.parse(req.url, true);
 				var query = url_parts.query;
@@ -33,7 +37,72 @@ class Inforia {
 			}
 		);
 
-		server.listen(1337, "localhost");
+		var io : SocketIO = Node.require('./socket.io').listen(8008);
+
+		// wrap socket.io with basic identification and message queueing
+		// code is in lib/socket_manager.js
+		//var socket_managerB : SocketManagerBuilder = Node.require("socket_manager");
+		//var socket_manager = socket_managerB.create(io);
+
+		var chat = io.sockets.on('connection', function (socket : Socket) {
+			socket.emit('news', { hello: 'world' });
+			// chat.emit('a message', { everyone: 'in', '/chat': 'will get' });
+		});
+
+		/*/ use xhr-polling as the transport for socket.io
+		io.configure(function () {
+			io.set('transports', ['xhr-polling']);
+			io.set('polling duration', 10);
+		}); //
+		var ws = SocketIo.listen(80);
+		ws.of(SocketNamespace).on('connection',
+			function (socket : SocketNamespace) {
+				socket.on('set nickname',
+					function (name) {
+						socket.set('nickname', name,
+							function () {
+								socket.emit('ready');
+							}
+						);
+					}
+				);
+				socket.on('msg',
+					function () {
+						socket.get('nickname',
+							function (err, name) {
+								console.log('Chat message by ', name);
+							}
+						);
+					}
+				);
+			}
+		);
+
+		/*var ws = SocketIo.listen(80);
+		ws.sockets.on('connection',
+			function (socket : SocketNamespace) {
+				socket.on('set nickname',
+					function (name) {
+						socket.set('nickname', name,
+							function () {
+								socket.emit('ready');
+							}
+						);
+					}
+				);
+				socket.on('msg',
+					function () {
+						socket.get('nickname',
+							function (err, name) {
+								console.log('Chat message by ', name);
+							}
+						);
+					}
+				);
+			}
+		); // */
+
+		app.listen(1337, "localhost");
 		trace('Server running at http://127.0.0.1:1337/');
 	}
 }
